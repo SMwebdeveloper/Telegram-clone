f4555c028f288492675c38ec834bde4a350a7fc8
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2024-11-21 16:31:19 +0500

    add telegram clone

---
+++ client/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import "./globals.css";
+
+const spaceGrotesk = Space_Grotesk({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+});
+export const metadata: Metadata = {
+  title: "Telegram app",
+  description: "Telegram web application clone created by Samandar",
+  icons: {
+    icon: "/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${spaceGrotesk.variable} antialiased`}
+        suppressHydrationWarning
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
