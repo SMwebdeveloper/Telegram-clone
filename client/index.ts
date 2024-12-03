9e553a099ca4205287d1360927b2c8eba675953c
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2024-12-03 17:03:29 +0500

    add server

--- client/index.ts
+++ client/index.ts
@@ -2,7 +2,7 @@ export interface IUser {
   email: string;
   _id: number;
   avatar: string;
-  firstName: string;
-  lastName: string;
-  bio: string;
+  firstName?: string;
+  lastName?: string;
+  bio?: string;
 }
