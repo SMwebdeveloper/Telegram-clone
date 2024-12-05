7ad4e338ce2f6c13fcd125c0304ec038176a062d
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2024-12-05 22:49:08 +0500

    fixed server functions

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,19 @@
+const BaseError = require("../errors/base.error")
+const userModal = require("../models/user.models")
 class AuthController {
     async login(req, res, next) {
-        const { email } = req.body
-        res.json({ email })
+        try {
+            const { email } = req.body
+            const existUser = await userModal.findOne({ email })
+
+            if (existUser) {
+                throw BaseError.BadRequest("User already exist", [{ email: "User already exist" }])
+            }
+            const createUser = await userModal.create({ email })
+            res.status(201).json(createUser)
+        } catch (error) {
+            next(error)
+        }
     }
     async verify(req, res, next) {
         const { email, otp } = req.body
