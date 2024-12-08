6b2a71e6fcfa16cd2f2ef01259ea11ca6d38a99a
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2024-12-08 12:42:54 +0500

    finish server

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,6 +2,7 @@ const BaseError = require('../errors/base.error')
 const { CONST } = require('../lib/constants')
 const messageModels = require('../models/message.models')
 const userModels = require('../models/user.models')
+const mailService = require('../service/mail.service')
 
 class UserController {
     // [GET]
@@ -58,12 +59,27 @@ class UserController {
     // [POST]
     async createMessage(req, res, next) {
         try {
-            const newMessage = await messageModels.create(req.body)
-            const currentMessage = await messageModels
+            const newMessage = await messageModel.create(req.body)
+            const currentMessage = await messageModel
                 .findById(newMessage._id)
                 .populate({ path: 'sender', select: 'email' })
                 .populate({ path: 'receiver', select: 'email' })
             res.status(201).json({ newMessage: currentMessage })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async messageRead(req, res, next) {
+        try {
+            const { messages } = req.body
+            const allMessages = []
+
+            for (const message of messages) {
+                const updatedMessage = await messageModels.findByIdAndUpdate(message._id, { status: CONST.READ }, { new: true })
+                allMessages.push(updatedMessage)
+            }
+
+            res.status(200).json({ messages: allMessages })
         } catch (error) {
             next(error)
         }
@@ -74,16 +90,90 @@ class UserController {
             const userId = '675493543b7fbe3c33f1401e'
             const user = await userModels.findById(userId)
             const contact = await userModels.findOne({ email })
-            if (!contact) throw BaseError.BadRequest("User with this email does not exist")
+            if (!contact) throw BaseError.BadRequest('User with this email does not exist')
 
-            if (user.email === contact.email) throw BaseError.BadRequest("You cannot add yourself as a contact")
+            if (user.email === contact.email) throw BaseError.BadRequest('You cannot add yourself as a contact')
 
             const existingContact = await userModels.findOne({ _id: userId, contacts: contact._id })
-            if (existingContact) throw new BaseError.BadRequest("Cannot already exists")
+            if (existingContact) throw BaseError.BadRequest('Contact already exists')
 
             await userModels.findByIdAndUpdate(userId, { $push: { contacts: contact._id } })
-            const addContact = await userModels.findByIdAndUpdate(contact._id, { $push: { contacts: userId } }, { new: true })
-            return res.status(201).json({ message: "Contact added successfully", contact: addContact })
+            const addedContact = await userModels.findByIdAndUpdate(contact._id, { $push: { contacts: userId } }, { new: true })
+            return res.status(201).json({ message: 'Contact added successfully', contact: addedContact })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async createReaction(req, res, next) {
+        try {
+            const { messageId, reaction } = req.body
+            const updatedMessage = await messageModels.findByIdAndUpdate(messageId, { reaction }, { new: true })
+            res.status(201).json({ updatedMessage })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async sendOtp(req, res, next) {
+        try {
+            const { email } = req.body
+            const existingUser = await userModels.findOne({ email })
+            if (existingUser) throw BaseError.BadRequest('User with this email already exists')
+            await mailService.sendOtp(email)
+            res.status(200).json({ message: 'OTP sent successfully' })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    // [PUT]
+    async updateProfile(req, res, next) {
+        try {
+            const { userId, ...payload } = req.body
+            await userModels.findByIdAndUpdate(userId, payload)
+            res.status(200).json({ message: 'Profile updated successfully' })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async updateMessage(req, res, next) {
+        try {
+            const { messageId } = req.params
+            const { text } = req.body
+            const updatedMessage = await messageModels.findByIdAndUpdate(messageId, { text }, { new: true })
+            res.status(200).json({ updatedMessage })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async updateEmail(req, res, next) {
+        try {
+            const { email, otp } = req.body
+            const result = await mailService.verifyOtp(email, otp)
+            if (result) {
+                const userId = '675493543b7fbe3c33f1401e'
+                const user = await userModels.findByIdAndUpdate(userId, { email }, { new: true })
+                res.status(200).json({ user })
+            }
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    // [DELETE]
+    async deleteUser(req, res, next) {
+        try {
+            const userId = '675493543b7fbe3c33f1401e'
+            await userModels.findByIdAndDelete(userId)
+            res.status(200).json({ message: 'User deleted successfully' })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async deleteMessage(req, res, next) {
+        try {
+            const { messageId } = req.params
+            await messageModels.findByIdAndDelete(messageId)
+            res.status(200).json({ message: 'Message deleted successfully' })
         } catch (error) {
             next(error)
         }
--- server/routes/index.js
+++ server/routes/index.js
@@ -15,7 +15,17 @@ router.group('/user', route => {
     route.get('/contacts', userController.getContact)
 
     route.post('/message', userController.createMessage)
+    route.post('/message-read', userController.messageRead)
     route.post('/contact', userController.createContact)
+    route.post('/reaction', userController.createReaction)
+    route.post('/send-otp', userController.sendOtp)
+
+    route.put('/profile', userController.updateProfile)
+    route.put('/message/:messageId', userController.updateMessage)
+    route.put('/email', userController.updateEmail)
+
+    route.delete('/', userController.deleteUser)
+    route.delete('/message/:messageId', userController.deleteMessage)
 })
 
 module.exports = router
